7cfec929d68ed538e06c04e902473c7f3ad5c1f5
Tyler Garlick <tjgarlick@gmail.com>
2013-12-06 11:58:30 -0500

    fixed wireup

--- Src/FlitBit.IoC.Owin.Tests/Wireup.cs
+++ Src/FlitBit.IoC.Owin.Tests/Wireup.cs
@@ -1,12 +1,11 @@
-﻿using FlitBit.IoC.Owin.Tests;
-using FlitBit.Wireup;
+﻿using FlitBit.Wireup;
 using FlitBit.Wireup.Meta;
 
 [assembly: WireupDependency(typeof(FlitBit.IoC.Owin.AssemblyWireup))]
-[assembly: Wireup(typeof(Wireup))]
+[assembly: Wireup(typeof(FlitBit.IoC.Owin.Tests.AssemblyWireup))]
 namespace FlitBit.IoC.Owin.Tests
 {
-    public class Wireup : IWireupCommand
+    public class AssemblyWireup : IWireupCommand
     {
         public void Execute(IWireupCoordinator coordinator)
         {
